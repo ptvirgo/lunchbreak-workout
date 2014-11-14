95d1f36187a320621da2307dc9228906ffee1cf2
Pablo Virgo <pv@pablovirgo.com>
2014-11-14 15:21:36 -0500

    Web App is now a single file, templates in __DATA__

--- web-mission.pl
+++ web-mission.pl
@@ -105,3 +105,126 @@ get '/:equipped' => sub {
 
 
 app->start();
+
+__DATA__
+
+@@ index.html.ep
+<!DOCTYPE html>
+<html>
+<head>
+  <title><%= $title %></title>
+  <link rel="stylesheet" type="text/css" href="/style/workout.css">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <% for my $link (@{ $navigation }) { %>
+      <li><a href="/<%= lc($link) %>"><%= ucfirst($link) %></a></li>
+      <% } %>
+    </ul>
+  </nav>
+  <main>
+    <h1><%= $title %></h1>
+    <%== $content %>
+  </main>
+  <footer>2014 &copy; Pablo Virgo</footer>
+</body>
+</html>
+
+@@ not_found.html.ep
+
+<% my $title = "404 Not Found"; %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title><%= $title %></title>
+  <link rel="stylesheet" type="text/css" href="/style/workout.css">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+    </ul>
+  </nav>
+  <main>
+    <h1><%= $title %></h1>
+    <p>There is no such page. Maybe stick to the links that work.</p>
+  </main>
+  <footer>2014 &copy; Pablo Virgo</footer>
+</body>
+</html>
+
+@@ style.css.ep
+body {
+  color: #222;
+  font-family: 'Liberation Sans', sans-serif; 
+}
+
+a {
+  color: #474;
+  font-weight: bold;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #393;
+  text-decoration: underline;
+}
+
+a:visited {
+  color: #376;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  margin: 0 0 .5em 0;
+  padding: 0;
+}
+
+p {
+  margin: 0 0 1em 0;
+  padding: 0;
+}
+
+main {
+  background-color: #eee;
+  border-left: 1px solid #222;
+  border-top: 1px solid #222;
+  border-right: 3px solid #222;
+  border-bottom: 3px solid #222;
+  border-bottom-left-radius: .25em;
+  border-top-right-radius: .5em;
+  border-bottom-right-radius: .25em;
+  margin: 0;
+  padding: .5em 1ex;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline;
+  border-left: 1px solid #222;
+  border-top: 1px solid #222;
+  border-right: 3px solid #222;
+  border-top-right-radius: .5em;
+  border-top-left-radius: .5em;
+  overflow: hidden;
+  padding: 0 2ex 0 .5ex;
+}
+
+nav ul li a:hover {
+  text-decoration: none;
+}
+
+nav ul li:hover {
+  background-color: #dfd;
+}
+
+footer {
+  font-size: small;
+  text-align: right;
+}
